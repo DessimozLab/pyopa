bf42d40dc75a53bdb9ba6fd0fa37dc5bafde02f0
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2015-05-21 14:18:26 +0200

    extended setup.py for publication in pypi

--- setup.py
+++ setup.py
@@ -20,24 +20,34 @@ setup(
 	    Extension(
 		package_name,
 		sources=[package_name+'.pyx'] + map(lambda c: os.path.join(c_dir, c), c_sources),
-		#libraries=["swps3"],
-		#extra_link_args=["-L../"],
-        #extra_compile_args = ['-DPY_DEBUG'],
 		include_dirs=[c_dir, numpy.get_include()]
 	)]
     ),
     name=package_name,
     version='0.6',
-    author='Your Name',
-    author_email='your@email',
-    #cmdclass = {'build_ext': build_ext},
-    #install_requires=['numpy', 'cython'],
+    author='OMA Browser',
+    author_email='contact@omabrowser.org',
+    install_requires=['numpy', 'cython'],
     data_files=[
         (os.path.join(data_dir, 'data'), ['test/data/reference_test_results.dat', 'test/data/testseqs.txt']),
         (os.path.join(data_dir, 'data/matrices/json'),
          ['test/data/matrices/json/all_matrices.json', 'test/data/matrices/json/logPAM1.json']),
         (data_dir, ['test/runall.py', 'test/test_darwin.py', 'test/test_input.py'])
     ],
-    license='MIT',
-    description='Example package that says hello'
+    license='MPL 2.0',
+    classifiers = [
+         'Development Status :: 3 - Alpha',
+         'Intended Audience :: Developers',
+         'Intended Audience :: Science/Research',
+         'Topic :: Scientific/Engineering :: Bio-Informatics',
+         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+         'Programming Language :: Python :: 2',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.3',
+         'Programming Language :: Python :: 3.4',
+         ],
+    keywords='sequence alignments Smith-Waterman dynamic programming',
+    description='pyopa computes optimal pairwise sequence alignments using a vectorized implementation of the Smith-Waterman algorithm',
+    url='http://omabrowser.org/',
 )

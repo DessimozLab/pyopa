0d3f2248d29d2618b3664d8a05baeae9f6838de3
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2018-11-14 16:05:38 +0100

    wrong requirements file

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -6,7 +6,7 @@ set -e -x
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
